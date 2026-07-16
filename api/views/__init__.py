8347cfc4d0e979b305d21ce2432ccc568102fb34
Achmad Fadilah <acmadfadilah811@gmail.com>
2026-07-16 15:52:20 +0700

    Commit 2: Pindahkan Domain WhatsApp

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -1,1 +1,6 @@
 from .legacy import *
+from .whatsapp import (
+    FonnteWebhookView, EvolutionWebhookView, WhatsAppStatusView,
+    WhatsAppChatsView, WhatsAppMessagesView, WhatsAppSendMessageView,
+    WhatsAppSendMediaView
+)
